dd0cd1db89772369b1bc0450bc1ad58b53aac34d
llizz.kh <lizakhan2001@mail.ru>
2025-10-13 16:27:25 +0300

    create app template

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Main component, that is connected to index.js
+ */
+
+export const App = () => {
+    return (
+        <div>
+            Hello!
+        </div>
+    );
+};
