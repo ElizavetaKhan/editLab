177ac6648e16497b811d2a204e435669c685acda
llizz.kh <lizakhan2001@mail.ru>
2026-01-14 23:49:17 +0300

    fix document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
 export default function Document() {
   return (
@@ -11,31 +10,4 @@ export default function Document() {
       </body>
     </Html>
   )
-}
-
-Document.getInitialProps = async (ctx) => {
-  const sheet = new ServerStyleSheet()
-  const originalRenderPage = ctx.renderPage
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(<App {...props} />),
-      })
-
-    const initialProps = await ctx.getInitialProps(ctx)
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    }
-  } finally {
-    sheet.seal()
-  }
 }
